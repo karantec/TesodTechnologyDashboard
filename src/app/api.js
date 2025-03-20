b60e49976dc9b75d0923d13ba8a1838a9731098c
karantec <karanrana3095@gmail.com>
2025-03-20 17:22:36 +0530

    commit

--- src/app/api.js
+++ src/app/api.js
@@ -2,8 +2,12 @@ import axios from 'axios';
 
 export const loginUser = async (email, password) => {
     try {
-        const response = await axios.post('https://framedigitalbackend.onrender.com/auth/login', { email, password });
-
+        const response = await axios.post(
+            'https://framedigitalbackend.onrender.com/auth/login',
+            { email, password },
+            { headers: { 'Content-Type': 'application/json' } }
+        );
+        
         if (response.data.token) {
             localStorage.setItem('token', response.data.token); // Store the token securely
         }
